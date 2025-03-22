ec9c4a1ec581f5152d300a34436da509b46fc38a
Mukhammad Karimov <10654965+heykarimoff@users.noreply.github.com>
2025-03-23 07:46:15 +1100

    dl with experience

--- run-70b-instruct-few-shot.sh
+++ run-70b-instruct-few-shot.sh
@@ -151,23 +151,23 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 
 ### CodeReviewer with Ownership DL ###
 
-# python code_review_instruction_parallel.py \
-#     --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
-#     --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
-#     --conf_path ../config/few-shot-dl-with-ownership/llama-31-70B-instruct-cr-pkg_aco_faiss-3.json \
-#     --temperature 0.0 --top_p 0.95 \
-#     --max_new_tokens 2048 \
-#     --tp_size 4 \
-#     --debug False
+python code_review_instruction_parallel.py \
+    --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
+    --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
+    --conf_path ../config/few-shot-dl-with-ownership/llama-31-70B-instruct-cr-pkg_aco_faiss-3.json \
+    --temperature 0.0 --top_p 0.95 \
+    --max_new_tokens 2048 \
+    --tp_size 4 \
+    --debug False
 
-# python code_review_instruction_parallel.py \
-#     --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
-#     --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
-#     --conf_path ../config/few-shot-dl-with-ownership/llama-31-70B-instruct-cr-pkg_rso_faiss-3.json \
-#     --temperature 0.0 --top_p 0.95 \
-#     --max_new_tokens 2048 \
-#     --tp_size 4 \
-#     --debug False
+python code_review_instruction_parallel.py \
+    --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
+    --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
+    --conf_path ../config/few-shot-dl-with-ownership/llama-31-70B-instruct-cr-pkg_rso_faiss-3.json \
+    --temperature 0.0 --top_p 0.95 \
+    --max_new_tokens 2048 \
+    --tp_size 4 \
+    --debug False
 
 # ### CodeReviewerNew IR ###
 
@@ -198,14 +198,14 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 #     --tp_size 4 \
 #     --debug False
 
-python code_review_instruction_parallel.py \
-    --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
-    --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
-    --conf_path ../config/few-shot-ir/llama-31-70B-instruct-crn-bm25-4.json \
-    --temperature 0.0 --top_p 0.95 \
-    --max_new_tokens 2048 \
-    --tp_size 4 \
-    --debug False
+# python code_review_instruction_parallel.py \
+#     --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
+#     --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
+#     --conf_path ../config/few-shot-ir/llama-31-70B-instruct-crn-bm25-4.json \
+#     --temperature 0.0 --top_p 0.95 \
+#     --max_new_tokens 2048 \
+#     --tp_size 4 \
+#     --debug False
 
 ### CodeReviewerNew with Ownership IR ###
 
@@ -258,23 +258,23 @@ python code_review_instruction_parallel.py \
 
 ### CodeReviewerNew with Ownership DL ###
 
-# python code_review_instruction_parallel.py \
-#     --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
-#     --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
-#     --conf_path ../config/few-shot-dl-with-ownership/llama-31-70B-instruct-crn-pkg_aco_faiss-3.json \
-#     --temperature 0.0 --top_p 0.95 \
-#     --max_new_tokens 2048 \
-#     --tp_size 4 \
-#     --debug False
+python code_review_instruction_parallel.py \
+    --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
+    --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
+    --conf_path ../config/few-shot-dl-with-ownership/llama-31-70B-instruct-crn-pkg_aco_faiss-3.json \
+    --temperature 0.0 --top_p 0.95 \
+    --max_new_tokens 2048 \
+    --tp_size 4 \
+    --debug False
 
-# python code_review_instruction_parallel.py \
-#     --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
-#     --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
-#     --conf_path ../config/few-shot-dl-with-ownership/llama-31-70B-instruct-crn-pkg_rso_faiss-3.json \
-#     --temperature 0.0 --top_p 0.95 \
-#     --max_new_tokens 2048 \
-#     --tp_size 4 \
-#     --debug False
+python code_review_instruction_parallel.py \
+    --ckpt_dir ./meta-llama/Llama-3.1-70B-Instruct \
+    --tokenizer_path ./meta-llama/Llama-3.1-70B-Instruct \
+    --conf_path ../config/few-shot-dl-with-ownership/llama-31-70B-instruct-crn-pkg_rso_faiss-3.json \
+    --temperature 0.0 --top_p 0.95 \
+    --max_new_tokens 2048 \
+    --tp_size 4 \
+    --debug False
 
 ##DO NOT ADD/EDIT BEYOND THIS LINE##
 ##Job monitor command to list the resource usage
